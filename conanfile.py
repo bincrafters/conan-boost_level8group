ca229720b4a595ba6b2e735bc8302ba6f9ba0ded
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 01:59:47 -0400

    add shared option change source method append BOOST_ALL_NO_LIB

--- conanfile.py
+++ conanfile.py
@@ -12,6 +12,8 @@ class BoostLevel8GroupConan(ConanFile):
     license = "www.boost.org/users/license.html"
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing"
     lib_short_names = ["math", "lexical_cast"]
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     requires = "Boost.Array/1.64.0@bincrafters/testing", \
                "Boost.Assert/1.64.0@bincrafters/testing", \
                "Boost.Atomic/1.64.0@bincrafters/testing", \
@@ -44,21 +46,24 @@ class BoostLevel8GroupConan(ConanFile):
     # exception2 type_traits3
 
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + version        
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name))
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
-                 
     def build(self):
         self.run(self.deps_user_info['Boost.Generator'].b2_command)
-        
+
     def package(self):
+        self.copy(pattern="*", dst="lib", src="stage/lib")
         for lib_short_name in self.lib_short_names:
             include_dir = os.path.join(lib_short_name, "include")
             self.copy(pattern="*", dst="include", src=include_dir)
 
-        self.copy(pattern="*", dst="lib", src="stage/lib")
-       
     def package_info(self):
-        self.user_info.lib_short_names = (",").join(self.lib_short_names)
+        self.user_info.lib_short_names = ",".join(self.lib_short_names)
         self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
+
