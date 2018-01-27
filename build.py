e98859724ce79d6e23c0bf04888481ae50a91662
Jerry W <jerrywiltse@gmail.com>
2018-01-27 09:14:23 -0500

    try pure_c=True temp to get clang to work[skip ci]

--- build.py
+++ build.py
@@ -5,5 +5,5 @@ from bincrafters import build_template_boost_default
 
 
 if __name__ == "__main__":
-    builder = build_template_boost_default.get_builder()
+    builder = build_template_boost_default.get_builder(args="--build=missing", pure_c=True)
     builder.run()
