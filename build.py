b5051d5204e8332d831292dac22756bff5a6da20
Jerry W <jerrywiltse@gmail.com>
2018-01-27 11:51:37 -0500

    roll back to pure_c=True, and no splitting debug/release

--- build.py
+++ build.py
@@ -5,5 +5,5 @@ from bincrafters import build_template_boost_default
 
 
 if __name__ == "__main__":
-    builder = build_template_boost_default.get_builder(args="--build=missing", pure_c=True)
+    builder = build_template_boost_default.get_builder(pure_c=True)
     builder.run()
